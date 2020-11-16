e4709bced08d11ddc8669d831e59798098f7cbc1
CWAGrant <cwagrant@gmail.com>
2020-11-16 15:34:31 -0600

    update version number

--- Core.lua
+++ Core.lua
@@ -1,5 +1,5 @@
 EasyDestroy = {}
-EasyDestroy.Version =  "1.0.2"
+EasyDestroy.Version =  "1.0.3"
 EasyDestroy.DebugActive = false
 EasyDestroy.AddonName = "EasyDestroy"
 EasyDestroy.AddonLoaded = false
